c50d6976f06d698f99eedd85ecffa26b10a1028c
Juan David Cano <juand-canov@unilibre.edu.co>
2021-06-16 15:59:05 +0000

    adicionar README.md

---
+++ README.m
@@ -0,0 +1,4 @@
+# Repositorio de Notebooks Jupyter
+## Juan David Cano 
+
+En este repositorio vamos a mantener los cuadernos del diplomado Ciencia de datos
---
+++ README.md
@@ -0,0 +1,4 @@
+# Repositorio de Notebooks Jupyter
+## Juan David Cano
+
+Aqui tendremos todos los cuadernos del diplomado
